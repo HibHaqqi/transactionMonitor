e923642c9b3063e89ab505d7a463456ff085a096
hibhaqqi <haqqi09@gmail.com>
2023-11-28 21:40:28 +0700

    setup claudinary

--- libs/upload-cloudinary.js
+++ libs/upload-cloudinary.js
@@ -3,12 +3,15 @@ const fs = require('fs');
 const cloudinary = require('cloudinary').v2;
 
 require('dotenv').config();
+const CLOUDINARY_CLOUD_NAME = 'dbgmovl5g';
+const CLOUDINARY_API_KEY = '297236666458235';
+const CLOUDINARY_API_SECRET = 'jhmlweyjbf30yeNEVVOTv975c1M';
 
           
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET  
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
 });
 
 const uploadCloudinary = async (filePath) => {
